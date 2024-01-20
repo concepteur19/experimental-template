fb6961eba190e9feafcb8a43f2fe0baa80bb089f
concepteur19 <zobel.tchomgui@gmail.com>
2024-01-20 19:55:53 +0100

    first push

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import '../src/styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Experimental Template</h1>
+      
+    </div>
+  );
+}
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+//fonts exo2, isidora & poiretOne
+import './fonts/Exo2-Bold.ttf';
+import './fonts/Exo2-Regular.ttf';
+import './fonts/Exo2-Light.ttf';
+import './fonts/Exo2-SemiBold.ttf';
+import './fonts/isidora-bold.otf';
+import './fonts/isidora-medium.otf';
+import './fonts/isidora-regular.otf';
+import './fonts/PoiretOne-Regular.ttf';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
